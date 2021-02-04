5fa49f1b69bb5ae059f33662b626b98164e341a6
Ryan Frisch <nathanielrfrisch@gmail.com>
2021-02-03 23:42:30 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieSite.Models;
+
+namespace MovieSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult My_Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Enter_Movies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Enter_Movies(Movie movieEntered)
+        {
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddMovie(movieEntered);
+            }
+            return View();
+        }
+        public IActionResult List_Movies()
+        {
+            return View(TempStorage.Movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieSite.Models
+{
+    public class Movie
+    {
+        [Required(ErrorMessage ="You need to enter a Category!")]
+        public string Category { get; set; }
+        [Required(ErrorMessage = "You need to enter a Title!")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "You need to enter a Year!")]
+        public int? Year { get; set; }
+        [Required(ErrorMessage = "You need to enter a Director!")]
+        public string Director { get; set; }
+        [Required(ErrorMessage = "You need to enter a Rating!")]
+        public string Rating { get; set; }
+
+        public bool? Edited { get; set; }
+        public string Lent_To { get; set; }
+        [MaxLength(120)]
+        public string Notes { get; set; }
+
+    }
+}
